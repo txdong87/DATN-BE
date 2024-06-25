aaaf1c14ccf0212c3d5388cf49d9c018a470d39d
txdong87 <tranxuandonghy@gmail.com>
2024-06-26 03:16:32 +0700

    fic

---
+++ Application/DTOs/MedicalCdhaCaseStudyDTO/AddMedicalCdhaCaseStudyDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.MedicalCdhaCaseStudyDTO
+{
+    public class AddMedicalCdhaCaseStudyDto
+    {
+        public int? DoctorId { get; set; }
+        public int? KtvId { get; set; }
+        public int? CaseStudyId { get; set; }
+        public List<MedicalCdha> MedicalCdha { get; set; }
+    }
+}
